c42d0b267b68b44d84d17151893f87052cdad5ad
Araz1998 <33088732+Araz1998@users.noreply.github.com>
2020-09-04 21:12:07 +0300

    CRUD app on S

--- src/main/java/com/araz/controller/UserController.java
+++ src/main/java/com/araz/controller/UserController.java
@@ -2,11 +2,15 @@ package com.araz.controller;
 
 import com.araz.model.User;
 import com.araz.service.UserService;
+import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.persistence.Id;
 import java.util.List;
 
 @Controller
@@ -31,7 +35,27 @@ public class UserController {
         return "user-create";
      }
 
+     @PostMapping("/user-create")
      public String createUser(User user){
+        userService.saveUser(user);
+        return "redirect:/users";
+     }
+
+     @GetMapping("/user-delete/{id}")
+     public String deleteUser( @PathVariable ("id") Long id){
+        userService.deleteById(id);
+        return "redirect:/users";
+     }
+
+     @GetMapping("/user-update/{id?")
+     public String updateUsers( @PathVariable ("id") Long id, Model model){
+        User user = userService.findById(id);
+        model.addAttribute("user", user);
+        return "/user-update";
+     }
+
+     @PostMapping("user-update/{id}")
+     public String uodateUser(User user){
         userService.saveUser(user);
         return "redirect:/users";
      }
